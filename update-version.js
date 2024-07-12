433ef0a4752f8e5098b5c7240f206dd25fa37d90
Monika <monika.rai@madebyaura.com>
2024-07-12 16:12:20 +0530

    Fix version script to include PR ID and commit hash

--- update-version.js
+++ update-version.js
@@ -2,30 +2,40 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const GITHUB_API_URL = 'https://api.github.com/repos/raimonika20/update-style-version/commits';
-const PR_API_URL = 'https://api.github.com/repos/raimonika20/update-style-version/pulls';
+const REPO_OWNER = 'raimonika20';
+const REPO_NAME = 'update-style-version';
+const GITHUB_API_URL = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits`;
+const PR_API_URL = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls`;
 const STYLE_CSS_PATH = path.join(__dirname, 'style.css');
+
+async function getLatestCommitHash() {
+    const response = await axios.get(GITHUB_API_URL);
+    return response.data[0].sha.substring(0, 7);
+}
+
+async function getLatestPRID(commitHash) {
+    const response = await axios.get(PR_API_URL);
+    const prs = response.data;
+    for (const pr of prs) {
+        const prCommits = await axios.get(pr.commits_url);
+        if (prCommits.data.some(commit => commit.sha.startsWith(commitHash))) {
+            return pr.number;
+        }
+    }
+    throw new Error('No PR found for the latest commit.');
+}
 
 async function updateVersion() {
     try {
-        // Fetch the latest commit hash from GitHub API
-        const response = await axios.get(GITHUB_API_URL);
-        const latestCommitHash = response.data[0].sha.substring(0, 7); // Get first 7 characters of the hash
+        const latestCommitHash = await getLatestCommitHash();
+        const latestPRID = await getLatestPRID(latestCommitHash);
 
-        // Fetch the latest PR ID
-        const prResponse = await axios.get(PR_API_URL);
-        const latestPR = prResponse.data[0]; // Assuming the latest PR is the first in the list
-        const latestPRID = latestPR.number;
-
-        // Read the style.css file
         let styleCss = fs.readFileSync(STYLE_CSS_PATH, 'utf8');
 
-        // Check for existing hash and update the version in style.css
         styleCss = styleCss.replace(/(\* Version:\s*\d+\.\d+\.\d+)(-\w+)?/, (match, p1) => {
             return `${p1}-pr${latestPRID}-${latestCommitHash}`;
         });
 
-        // Write the updated style.css back to the file
         fs.writeFileSync(STYLE_CSS_PATH, styleCss, 'utf8');
 
         console.log('style.css updated successfully.');
