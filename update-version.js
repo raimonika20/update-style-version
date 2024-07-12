5c372c58b2d59106efc3ca288e4548707a9b398a
Monika <monika.rai@madebyaura.com>
2024-07-12 13:27:17 +0530

    Initial Release

---
+++ update-version.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const GITHUB_API_URL = 'https://api.github.com/repos/raimonika20/update-style-version/commits';
+const STYLE_CSS_PATH = path.join(__dirname, 'style.css');
+
+async function updateVersion() {
+    try {
+        // Fetch the latest commit hash from GitHub API
+        const response = await axios.get(GITHUB_API_URL);
+        const latestCommitHash = response.data[0].sha.substring(0, 7); // Get first 7 characters of the hash
+
+        // Read the style.css file
+        let styleCss = fs.readFileSync(STYLE_CSS_PATH, 'utf8');
+
+        // Update the version in style.css
+        styleCss = styleCss.replace(/(\* Version:\s*)([\d.]+)(.*)/, (match, p1, p2, p3) => {
+            return `${p1}${p2}-${latestCommitHash}${p3}`;
+        });
+
+        // Write the updated style.css back to the file
+        fs.writeFileSync(STYLE_CSS_PATH, styleCss, 'utf8');
+
+        console.log('style.css updated successfully.');
+    } catch (error) {
+        console.error('Error updating style.css:', error);
+    }
+}
+
+updateVersion();
