41d2c3bac08a2ec85ed2ac4e548b88930efdfc67
Monika <monika.rai@madebyaura.com>
2024-07-15 17:05:44 +0530

    Remove Comment1

--- update-version.js
+++ update-version.js
@@ -64,7 +64,7 @@ async function updateVersion() {
             versionSuffix = `${branchName}-${latestCommitHash}`;
         }
 
-        // comment
+      
         
 
         let styleCss = fs.readFileSync(STYLE_CSS_PATH, 'utf8');
